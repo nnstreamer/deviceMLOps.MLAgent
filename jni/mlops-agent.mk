df8abab84ce27ec6c972b105e1104e972b1a229e
hyunil park <hyunil46.park@samsung.com>
2025-01-17 11:19:37 +0900

    [sqlite] Add sqlite library
    Add sqlite lib and header file
    Modify .mk
    
    Signed-off-by: hyunil park <hyunil46.park@samsung.com>

--- jni/mlops-agent.mk
+++ jni/mlops-agent.mk
@@ -2,8 +2,19 @@ ifndef MLOPS_AGENT_ROOT
 $(error MLOPS_AGENT_ROOT is not defined!)
 endif
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := sqliteX
+LOCAL_SRC_FILES := $(MLOPS_AGENT_ROOT)/libsqliteX/$(APP_ABI)/libsqliteX.so
+LOCAL_EXPORT_C_INCLUDES := $(MLOPS_AGENT_ROOT)/libsqliteX/include
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
 # mlops agent headers
 MLOPS_AGENT_INCLUDE := $(MLOPS_AGENT_ROOT)/daemon/include
 
 # mlops agent sources
 MLOPS_AGENT_SRCS := $(MLOPS_AGENT_ROOT)/daemon/mlops-agent-android.c
+LOCAL_SHARED_LIBRARIES := sqliteX
+include $(BUILD_SHARED_LIBRARY)
+
